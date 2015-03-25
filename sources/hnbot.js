b71d1536d1be7717da556264f93213e0262bfb3b
aboscoids <abosco@idsbonded.com>
2015-03-25 14:35:25 -0700

    Update hnbot.js

--- sources/hnbot.js
+++ sources/hnbot.js
@@ -2,12 +2,12 @@ var request = require('request');
 var async = require('async');
 var https = require('https');
 
-var BASE_URL = 'https://news.ycombinator.com/';
+var BASE_URL = 'https://news.google.com/news?pz=1&zx=gpf74cp73x84/';
 
 
 module.exports = {
 
-  aliases: ['hn', 'hackernews', 'hacker news'],
+  aliases: ['hn', 'googlenews', 'google news'],
 
   pingAndSend: function (callback) {
 
@@ -18,7 +18,7 @@ module.exports = {
 
       items = JSON.parse(items);
 
-      var botPayload = 'Current Hacker News homepage:\n'
+      var botPayload = 'Current Google News:\n'
 
       var index = 0;
       async.forEach(items, function(item, cb) {
